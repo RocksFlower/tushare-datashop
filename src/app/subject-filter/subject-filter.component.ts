282a706fb50de55a88dc7fad6bdb066d6054b73b
RocksFlower <57193106@qq.com>
2018-09-08 22:37:04 +0800

    Responsive for iphone678

--- src/app/subject-filter/subject-filter.component.ts
+++ src/app/subject-filter/subject-filter.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubjectFilterComponent implements OnInit {
 
+  private selectedType: string = "全部分类";
+  private selectedBrand: string = "全部品牌";
+  private selectedPrice: string = "全部价格";
+  private selectedFormat: string = "全部格式";
+  private selectedFrequency: string = "全部更新频率";
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  selectType(selectedType: string): void {
+    this.selectedType = selectedType;
+  }
+
+  selectBrand(selectedBrand: string): void {
+    this.selectedBrand = selectedBrand;
+  }
+
+  selectPrice(selectedPrice: string): void {
+    this.selectedPrice = selectedPrice;
+  }
+
+  selectFormat(selectedFormat: string): void {
+    this.selectedFormat = selectedFormat;
+  }
+
+  selectFrequency(selectedFrequency: string): void {
+    this.selectedFrequency = selectedFrequency;
+  }
 }
